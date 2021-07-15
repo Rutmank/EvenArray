23372b8b3584b685dc8b56e120d5cd7783e4a9b6
Rutmank <rutmank@yandex.ru>
2021-07-15 18:27:01 +0300

    Add project files.

---
+++ EvenArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EvenArray
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter any number greater than zero: ");
+            int count = int.Parse(Console.ReadLine());
+
+            int[] result = GetFirstEvenNumbers(count);
+
+            Console.WriteLine("Your numbers: ");
+
+            foreach (var item in result)
+            {
+                Console.Write(item.ToString() + " ");
+            }
+        }
+
+        public static int[] GetFirstEvenNumbers(int count)
+        {
+            int[] array = new int[count];
+            int evenNumber = 2;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = evenNumber;
+                evenNumber += 2;
+            }
+
+            return array;
+        }
+    }
+}
